e106ba6a6a1a24d85180c3ad7d77b27554503bef
bhagwat.gv@gmail.com <bhagwat.gv@gmail.com@150a96e8-9882-b39b-7b80-ab2e788e13de>
2013-04-29 20:32:00 +0000

    [gvb] starting the project.. base code to begin with

---
+++ src/Scenario.hpp
@@ -0,0 +1,32 @@
+#ifndef SCENARIO_HPP
+#define SCENARIO_HPP
+
+#include "Event.hpp"
+
+/**
+ * A general scenario to be simulated. This abstract class
+ * needs to be implemented by particular simulation scenarios.
+ */
+class Scenario 
+{
+
+	protected:
+		EventList& eventList;
+
+	public:
+		/**
+		 * Create a scenario with with a reference to the event list
+		 * used by the simulation.
+		 */
+		Scenario(EventList& eventList) : eventList(eventList) {}
+
+		/* Virtual descructor so that child classes can perform finishing */
+		virtual ~Scenario() {};
+		
+		/* Update statistics, if necessary */
+		virtual void updateStatistics(double time) = 0;
+
+};
+
+
+#endif
