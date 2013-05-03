d45626fbec29ce080854fe205fa1bc99aa5b8e18
bhagwat.gv@gmail.com <bhagwat.gv@gmail.com@150a96e8-9882-b39b-7b80-ab2e788e13de>
2013-05-03 01:14:31 +0000

    Passenger Request Event successful. On to next event

--- src/Car.cpp
+++ src/Car.cpp
@@ -14,6 +14,11 @@ Car::Car(int _id, Battery _battery, Location _location):
 	isHired = false;				
 }
 
+
+Car::Car(int _id, Battery _battery, Location _location, Distance dist, Revenue rev, bool isH):
+	id(_id),battery(_battery), currlocation(_location), cumDistance(dist), totalRevenue(rev), isHired(isH){
+}
+
 int Car::getCarId(){
 		return id;
 }
--- src/PassengerRequestEvent.cpp
+++ src/PassengerRequestEvent.cpp
@@ -3,18 +3,97 @@
 using namespace std;
 
 PassengerRequestEvent::PassengerRequestEvent(int _time, BaseScenario& _scenario, Location source, Location dest, int wait):
-		Event(time), baseScenario(_scenario), sourceLocation(source), destLocation(dest), waitingTime(wait){
-	time = _time;
-//	cout<<"time = "<<_time<<" source = "<<source.id<<" destination = "<<dest.id<<endl;
-	//cout<<"Now -->"<<endl;
-//	cout<<"time = "<<getTime()<<" source = "<<sourceLocation.id<<" destination = "<<destLocation.id<<endl<<"---"<<endl;
-
+    Event(time), baseScenario(_scenario), sourceLocation(source), destLocation(dest), waitingTime(wait) {
+    time = _time;
 }
 
-void PassengerRequestEvent::handle(EventList& eventList){
-	cout<<"Event handled at time "<<time<<endl;
+void PassengerRequestEvent::handle(EventList& eventList) {
 
-	cout<<"Number of cars is "<<baseScenario.cars.size()<<endl;
+    cout<<"Event handled at time "<<time<<endl;
+
+    std::vector<Car*> selectedCars;
+
+	int firstFilterCount = 0;
+
+    for(unsigned int i=0 ; i<baseScenario.cars.size() ; i++) {
+
+        int timeCarLocationToSource = baseScenario.leastTimeMatrix[ baseScenario.cars[i]->currlocation.id ][ sourceLocation.id ];
+
+        if( baseScenario.cars[i]->isHired == false && ( timeCarLocationToSource < waitingTime ) ) {
+
+			firstFilterCount++;
+
+            // cout << "First filter car Id = "<<baseScenario.cars[i]->getCarId()<<endl;
+
+            int distanceCarLocationToDestination = baseScenario.shortestDistanceMatrix[ sourceLocation.id ][ destLocation.id ] +
+                                                   baseScenario.shortestDistanceMatrix[ baseScenario.cars[i]->currlocation.id ][ sourceLocation.id ];
+
+            int currentCharge = baseScenario.cars[i]->battery.getCurrentCharge();
+
+            int DOD = baseScenario.cars[i]->battery.getDepthOfDischarge();
+
+            int chargeRequired = baseScenario.cars[i]->battery.chargeRequiredByDistance(distanceCarLocationToDestination);
+
+	        // cout<<" currCharge "<<currentCharge <<" DOD "<<DOD<<" chargeRequired "<<chargeRequired<<endl;
+            // cout<<" currCharge -DOD "<<currentCharge -DOD <<" chargeRequired "<<chargeRequired<<endl;
+            // cout<<" ----------"<<endl;
+
+
+            if( ( currentCharge - DOD ) > chargeRequired ) {
+
+               //  cout<<"Second Charge Filter"<<baseScenario.cars[i]->getCarId()<<endl;
+
+                int selId = baseScenario.cars[i]->getCarId();
+                Battery b (baseScenario.cars[i]->battery);
+                Location l (baseScenario.cars[i]->currlocation);
+                Distance d (baseScenario.cars[i]->cumDistance);
+                Revenue r(baseScenario.cars[i]->totalRevenue);
+
+                selectedCars.push_back(new Car(selId,b,l,d,r, false));
+
+            }
+        }
+    }
+
+    int minTime = 100000;
+
+    // cout<<"Number of selected cars after 1st Filter "<<firstFilterCount<<endl;
+    // cout<<"Number of selected cars after 2nd Filter "<<selectedCars.size()<<endl;
+
+    if(selectedCars.empty()) {
+        cout<<"No cars could pass even the Two fitlers :( "<<endl;
+        return;
+    }
+    else {
+
+
+        Car* choosenCar;
+
+        for(unsigned int i=0 ; i<selectedCars.size() ; i++) {
+
+            int timeCarLocationToSource=baseScenario.leastTimeMatrix[selectedCars[i]->currlocation.id][sourceLocation.id];
+
+            if( timeCarLocationToSource < minTime) {
+
+                minTime = timeCarLocationToSource;
+                choosenCar=selectedCars[i];
+            }
+        }
+
+        int choosenCarId=choosenCar->getCarId();
+
+        cout<<"Out of selected cars: "<<selectedCars.size()<<" Final 3rd Filter Choosen Car:"<<choosenCarId<<endl;
+
+		baseScenario.cars[choosenCarId]->isHired = true;
+
+		int distanceCarLocationToDestination =
+				baseScenario.shortestDistanceMatrix[ sourceLocation.id ][ destLocation.id ] +
+				baseScenario.shortestDistanceMatrix[ baseScenario.cars[choosenCarId]->currlocation.id ][ sourceLocation.id ];
+
+		baseScenario.cars[choosenCarId]->battery.dischargeBatteryByDistance( distanceCarLocationToDestination );
+        
+		//cout<<"Number of cars is was"<<baseScenario.cars.size()<<endl;
+    }
 }
 
 
